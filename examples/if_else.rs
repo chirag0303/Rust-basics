15c197d45eb1b96a816fbdaf57a81925f7f0e83c
chirag0303 <cheekugoyal0303@gmail.com>
2025-11-06 11:19:25 +0000

    Control flow

---
+++ examples/if_else.rs
@@ -0,0 +1,15 @@
+fn main(){
+    let x:i32 = 12;
+
+    let z:i32 = if x>0{
+        println!("Positive");
+        x
+    } else if x<0{
+        println!("Negative");
+        x
+    } else{
+        println!("Zero");
+        0
+    };
+
+}
